192bf61b8bec3360a2f7dfe8f60d54394bcf5380
arellanon <nahuel.arellano@gmail.com>
2024-09-28 15:53:04 -0300

    modificacion training.py se agrega duracion de procesamiento, modificacion de parametros de los modelos de clasificacion

--- training.py
+++ training.py
@@ -18,6 +18,7 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
 
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
@@ -35,83 +36,82 @@ from mne.channels import make_standard_montage
 from mne.preprocessing import (create_eog_epochs, create_ecg_epochs,
                                compute_proj_ecg, compute_proj_eog)
 
+from sklearn.metrics import make_scorer, precision_score, recall_score, f1_score
+
 from libb import *
 import logging
 
-def getResult(results, columns, file):
-    print('Mean Accuracy: %.3f' % results.best_score_)
-    print('Config: %s' % results.best_params_)
+import warnings
+warnings.filterwarnings("ignore")
+
+
+# Tipos de extraccion de caracteristicas
+def runCSP(file):
+    epochs=mne.read_epochs(file, proj=True, preload=True, verbose=None)
+    X = epochs.get_data()
+    y = epochs.events[:, -1]
+    sfreq=epochs.info['sfreq']
     
-    logging.info("Mean Accuracy: %.3f" % results.best_score_)
-    logging.info("Config: %s" % results.best_params_)
+    print("X: ", X.shape)
+    print("y: ", y.shape)
+
+    #Filtro banda 8 - 30 Hz
+    X=bandpass(X, 8, 30, sfreq)
     
-    df = pd.DataFrame(results.cv_results_)[
-        columns
+    csp = CSP(n_components=2, reg=None, log=True, norm_trace=False)
+
+    #Ejecuto csp
+    csp.fit(X, y)
+    # Transformar los datos
+    X_transform = csp.transform(X)
+    return X_transform, y
+
+def runPFBCSP(file):
+    frequency_bands = [
+        (4,8),   (6,10),   (8,12), (10,14), (12,16),
+        (14,18), (16,20), (18,22), (20,24), (22,26), 
+        (24,28), (26,30), (28,32), (30,34), (32,36),
+        (34,38), (36,40)
     ]
-    #new_column = file
-    df.insert(loc=0, column='File', value=file)
-    return df
-
-def getParamLDA():
-    param_solver1 = {
-        "LDA__solver": ["svd"],
-        "LDA__store_covariance": [True, False],
-        "LDA__tol": np.array([0.00001, 0.000001, 0.0000001, 0.00000001, 0.000000001, 0.0000000001, 0.00000000001])
-    }
     
-    param_solver2 = {
-        "LDA__solver": ["lsqr", "eigen"]
-        #"LDA__shrinkage": [ None, "auto"] + np.arange(0, 1, 0.01).tolist()
-    }
+    epochs=mne.read_epochs(file, proj=True, preload=True, verbose=None)
+    X = epochs.get_data()
+    y = epochs.events[:, -1]
+    sfreq=epochs.info['sfreq']
     
-    param_solver3 = {
-        "LDA__solver": ["lsqr"],
-        "LDA__shrinkage": [ None, "auto"] + np.arange(0, 1, 0.01).tolist()
-    }
+    X_train = X
+    y_train = y
     
-    #param_grid = [param_solver1, param_solver2]
-    #param_grid = param_solver3
-    param_grid = [param_solver1, param_solver3]
-    return param_grid
-
-def getParamCSP():
-    param_solver1 = {
-        "CSP__n_components": [2],
-        "CSP__reg": [None],
-        "CSP__log": [True, False],
-        "CSP__norm_trace": [True, False]
-    }
+    print("X: ", X.shape)
+    print("y: ", y.shape)
+        
+    # Crear lista para almacenar las características de cada banda de frecuencia
+    X_train_filtered = []
     
-    param_grid = param_solver1
-    return param_grid
-
-def getParamSVM():
-    param_solver1 = {
-        #"SVM__kernel": ["poly", "linear", "rbf", "sigmoid", "precomputed"]
-        "SVM__kernel": ["poly","linear","rbf","sigmoid"]
-    }
+    # Aplicar filtrado y CSP para cada banda de frecuencia
+    for band in frequency_bands:
+        fmin, fmax = band
+        print("fmin: ", fmin, " - fmax: ", fmax)
+        
+        # Filtrar épocas en la banda de frecuencia actual
+        epochs_band_train=bandpass(X_train, fmin, fmax, sfreq)
+        
+        # Aplicar CSP
+        csp = CSP(n_components=2, reg=None, log=True, norm_trace=False)
+        csp.fit(epochs_band_train, y_train)
+        
+        # Transformar los datos
+        X_train_band = csp.transform(epochs_band_train)
+        
+        # Agregar las características a las listas
+        X_train_filtered.append(X_train_band)
     
-    param_grid = param_solver1
-    return param_grid
+    # Combinar las características de todas las bandas de frecuencia
+    X_train_combined = np.concatenate(X_train_filtered, axis=1)
+    print("X_train_combined: ", X_train_combined.shape)
+    return X_train_combined, y
 
-def getParamTREE():
-    param_solver1 = {
-        "TREE__criterion": ["gini", "entropy", "log_loss"]
-    }
-    
-    param_grid = param_solver1
-    return param_grid
-
-def getParamRNA():
-    param_solver1 = {
-        "RNA__solver": ["lbfgs", "sgd", "adam"]
-    }
-    
-    param_grid = param_solver1
-    return param_grid
-
-
-def train_ptfbcsp(file):
+def runPTFBCSP(file):
     frequency_bands = [
         (4,8),   (6,10),   (8,12), (10,14), (12,16),
         (14,18), (16,20), (18,22), (20,24), (22,26), 
@@ -202,84 +202,89 @@ def train_ptfbcsp(file):
     return X_train_combined, y
 
 
-def train_pfbcsp(file):
-    frequency_bands = [
-        (4,8),   (6,10),   (8,12), (10,14), (12,16),
-        (14,18), (16,20), (18,22), (20,24), (22,26), 
-        (24,28), (26,30), (28,32), (30,34), (32,36),
-        (34,38), (36,40)
-    ]
+# Configuracion de parametros para los modelos.
+def getParamLDA():
+    param_solver1 = {
+        "LDA__solver": ["svd"],
+        'LDA__store_covariance': [True, False],
+        'LDA__n_components': [None, 1],
+        "LDA__tol": [1e-5, 1e-4, 1e-3, 1e-2, 1e-1]
+    }
     
-    epochs=mne.read_epochs(file, proj=True, preload=True, verbose=None)
-    X = epochs.get_data()
-    y = epochs.events[:, -1]
-    sfreq=epochs.info['sfreq']
-    
-    X_train = X
-    y_train = y
-    
-    print("X: ", X.shape)
-    print("y: ", y.shape)
-        
-    # Crear lista para almacenar las características de cada banda de frecuencia
-    X_train_filtered = []
-    
-    # Aplicar filtrado y CSP para cada banda de frecuencia
-    for band in frequency_bands:
-        fmin, fmax = band
-        print("fmin: ", fmin, " - fmax: ", fmax)
-        
-        # Filtrar épocas en la banda de frecuencia actual
-        epochs_band_train=bandpass(X_train, fmin, fmax, sfreq)
-        
-        # Aplicar CSP
-        csp = CSP(n_components=2, reg=None, log=True, norm_trace=False)
-        csp.fit(epochs_band_train, y_train)
-        
-        # Transformar los datos
-        X_train_band = csp.transform(epochs_band_train)
-        
-        # Agregar las características a las listas
-        X_train_filtered.append(X_train_band)
-    
-    # Combinar las características de todas las bandas de frecuencia
-    X_train_combined = np.concatenate(X_train_filtered, axis=1)
-    print("X_train_combined: ", X_train_combined.shape)
-    return X_train_combined, y
-    
-def train_csp(file):
-    epochs=mne.read_epochs(file, proj=True, preload=True, verbose=None)
-    X = epochs.get_data()
-    y = epochs.events[:, -1]
-    sfreq=epochs.info['sfreq']
-    
-    print("X: ", X.shape)
-    print("y: ", y.shape)
+    param_solver2 = {
+        "LDA__solver": ["lsqr", "eigen"],
+        'LDA__n_components': [None, 1],
+        "LDA__shrinkage": [None, 'auto', 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+    }    
+    param_grid = [param_solver1, param_solver2]
+    return param_grid
 
-    #Filtro banda 8 - 30 Hz
-    X=bandpass(X, 8, 30, sfreq)
-    
-    csp = CSP(n_components=2, reg=None, log=True, norm_trace=False)
+def getParamSVM():
+    param_grid = {
+        'SVM__C': [0.01, 0.1, 1, 10, 100],
+        'SVM__kernel': ['linear', 'rbf'],
+        'SVM__gamma': [0.01, 0.1, 1],
+        'SVM__class_weight': [None, 'balanced']
+    }
+    return param_grid
 
-    #Ejecuto csp
-    csp.fit(X, y)
-    # Transformar los datos
-    X_transform = csp.transform(X)
-    return X_transform, y
+def getParamKNN():
+    param_grid = {
+        'KNN__n_neighbors': [3, 5, 7, 9, 12],
+        'KNN__weights': ['uniform', 'distance'],
+        'KNN__metric': ['euclidean', 'manhattan', 'minkowski'],
+        'KNN__algorithm': ['auto', 'ball_tree']
+    }
+    return param_grid
 
-def train(file, fil_spatial, clf):
-    if fil_spatial =="CSP" :
-        X, y = train_csp(file)
-    elif fil_spatial == "PFBCSP" :
-        X, y = train_pfbcsp(file)
-    elif fil_spatial == "PTFBCSP" :
-        X, y = train_ptfbcsp(file)
+def getParamANN():
+    param_grid = {
+        'ANN__hidden_layer_sizes': [(50,), (100,), (50, 50), (100, 50)],
+        'ANN__activation': ['relu', 'tanh'],
+        'ANN__solver': ['adam', 'sgd'],
+        'ANN__learning_rate': ['constant', 'adaptive'],
+        'ANN__alpha': [0.0001, 0.001],
+        'ANN__max_iter': [200]
+    }
+    return param_grid
+
+# Retorna dataframe
+def getResult(results, columns, file,  method, classification, seconds_total):
+    print('Mean Accuracy: %.3f' % results.best_score_)
+    print('Config: %s' % results.best_params_)
     
-    print("Tranformacion X: ", X.shape)
-    print("Tranformacion y: ", y.shape)
+    logging.info("Mean Accuracy: %.3f" % results.best_score_)
+    logging.info("Config: %s" % results.best_params_)
     
-    columns =[ "params", "mean_test_score", "std_test_score"]
+    df = pd.DataFrame(results.cv_results_)[columns]
     
+    #df = pd.DataFrame(results.cv_results_)
+    #new_column = file
+    df.insert(loc=0, column='File', value=file)
+    df.insert(loc=1, column='method', value=method)
+    df.insert(loc=2, column='classification', value=classification)
+    df.insert(loc=3, column='seconds_total', value=seconds_total)
+    
+    
+    formatted_time = time.strftime("%H:%M:%S", time.gmtime(seconds_total))
+    print("Tiempo de ejecucion: ", formatted_time, "- segundos: ", seconds_total)
+    #logging.info("Tiempo de ejecucion: %.3f" % tduracion)
+    logging.info("Tiempo de ejecución: %s  - segundos:  %.3f" % (formatted_time, seconds_total))
+    
+    return df
+
+# Ejecucion de extraccion de caracteristicas
+def runFeatureExtraction(method, file):
+    if method =="CSP" :
+        X, y = runCSP(file)
+    elif method == "PFBCSP" :
+        X, y = runPFBCSP(file)
+    elif method == "PTFBCSP" :
+        X, y = runPTFBCSP(file)
+    return X, y
+
+# Devuelve modelo de clasificacion y sus parametros
+def getModel(clf):
     if clf == "LDA" :
         lda=LinearDiscriminantAnalysis()
         model = Pipeline([('LDA', lda)])
@@ -288,66 +293,116 @@ def train(file, fil_spatial, clf):
         svm = SVC(kernel='linear') # Linear Kernel
         model = Pipeline([('SVM', svm)])
         param_grid = getParamSVM()
-    elif clf == "TREE" :
-        tree = DecisionTreeClassifier(random_state=0)
-        model = Pipeline([('TREE', tree)])
-        param_grid = getParamTREE()
-    elif clf == "RNA" :
-        rna = MLPClassifier(random_state=1, max_iter=300)
-        model = Pipeline([('RNA', rna)])
-        param_grid = getParamRNA()
+    elif clf == "KNN" :
+        knn = KNeighborsClassifier(n_neighbors=3)
+        model = Pipeline([('KNN', knn)])
+        param_grid = getParamKNN()
+    elif clf == "ANN" :
+        ann = MLPClassifier(random_state=1, max_iter=200)
+        model = Pipeline([('ANN', ann)])
+        param_grid = getParamANN()
+    return model, param_grid
+
+def runProcess(file, method, classification):
+    #Tiempo de inicio
+    inicio = time.time()
+    print("\nFILE: ", file)
+    print("METODO DE EXTRACION DE CARACTERISTICAS: ", method)
+    print("MODELO DE CLASIFICACION: ", classification)
     
+    logging.info("--------------------------------------------------------------------------------------------------------")
+    logging.info("FILE: %s" % file)
+    logging.info("METODO DE EXTRACION DE CARACTERISTICAS: %s" % method)
+    logging.info("MODELO DE CLASIFICACION: %s" % classification)
+    
+    #Se realiza la extracion de caracteristicas
+    X, y = runFeatureExtraction(method, file)
+    
+    print("Tranformacion X: ", X.shape)
+    print("Tranformacion y: ", y.shape)
+    
+    logging.info("X: %s" % str(X.shape) )
+    logging.info("y: %s" % str(y.shape) )
+        
+    #Obtiene el modelo de clasificacion y sus parametros
+    model, param_grid = getModel(classification)
+        
+    #Cross-Validation 10x10
     cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=10, random_state=1)
     
-    #Busca la mejor configuracion
-    search = GridSearchCV(model, param_grid, scoring='accuracy', cv=cv, n_jobs=-1, verbose=-1)
-    results = search.fit(X, y)
+    #Metricas
+    scoring = {
+        'accuracy': 'accuracy',
+        'precision': make_scorer(precision_score),
+        'recall': make_scorer(recall_score),
+        'f1': make_scorer(f1_score)
+    }
     
-    df = getResult(results, columns, file)
+    #Busca la mejor configuracion
+    search = GridSearchCV(model, param_grid=param_grid, scoring=scoring, refit='accuracy', cv=cv, n_jobs=-1, verbose=-1)
+    results = search.fit(X, y)
+
+    #columns =[ "params", "mean_test_score", "std_test_score"]
+    columns =["mean_fit_time", "std_fit_time", "mean_score_time", "std_score_time", "params", "mean_test_accuracy", "std_test_accuracy", "mean_test_precision", "std_test_precision", "mean_test_recall", "std_test_recall", "mean_test_f1", "std_test_f1"]    
+    #columns =["mean_fit_time", "std_fit_time", "mean_score_time", "std_score_time", "params", "mean_test_accuracy", "std_test_accuracy", "mean_test_recall", "std_test_recall", "mean_test_f1", "std_test_f1"]    
+
     
     #Se guarda resultados y modelo
-
     #joblib.dump(search, path + filename + ".pkl")
-    logging.info('Finished')
-    return df
+    #Tiempo final
+    fin = time.time()
+    tduracion = fin-inicio
     
+    df = getResult(results, columns, file, method, classification, tduracion)
+    return df
+
     
 def run(folder_input, folder_output):
+    logging.info('input: %s ', folder_input)
     logging.info('output: %s ', folder_output)
-    fil_spatial = ["CSP","PFBCSP", "PTFBCSP"]
-    #fil_spatial = ["PTFBCSP"]
-    #clfs = ["LDA"]
-    clfs = ["LDA", "SVM", "TREE", "RNA"]
     
-    for fs in fil_spatial:
-        for clf in clfs:
-            firts = True
-            fout= folder_output + fs + "_" + clf + ".csv"
+    #Definimos las extracciones de caracteristicas a analizar
+    method_feature_extraction = ["CSP", "PFBCSP", "PTFBCSP"]
+    #method_feature_extraction = ["CSP"]
+    
+    #Definimos los modelos de clasificacion a analizar
+    #classifications = ["LDA", "SVM", "KNN", "ANN"]
+    classifications = ["LDA"]
+    
+    for method in method_feature_extraction:
+        first_method = True
+        fout_method = folder_output + method + ".csv"
+        for clf in classifications:
+            first_clf = True
+            fout_clf = folder_output + method + "_" + clf + ".csv"
             for root, dirs, files in os.walk(folder_input):
                 for file in files:
-                    filename=os.path.join(root, file)    
-                    df = train(filename, fs, clf)
-                    if firts :
-                        df.to_csv(fout, mode="w", index=False, header=True)
-                        firts = False
-                    else : 
-                        df.to_csv(fout, mode="a", index=False, header=False)
-    
+                    filename=os.path.join(root, file)
+                    df_clf = runProcess(filename, method, clf)
+                    
+                    #Guardamos las metricas para el method y clasificador
+                    df_clf.to_csv(fout_clf, mode="w" if first_clf else "a", index=False, header=first_clf)
+                    first_clf = False
+                    
+                    #Append datos
+                    try:
+                        df_method
+                    except NameError:
+                        # Si df no existe, simplemente asigna df_to_append
+                        df_method = df_clf
+                    else:
+                        # Si df ya existe, entonces lo appendea con df_to_append
+                        df_method = pd.concat([df_method, df_clf], ignore_index=True)
+            
+            df_method.to_csv(fout_method, mode="w" if first_method else "a", index=False, header=first_method)
+                             
 def main():
     logging.getLogger('mne').setLevel(logging.ERROR)
-    #columns_lda=["param_LDA__solver", "param_LDA__shrinkage", "param_LDA__store_covariance", "param_LDA__tol", "params", "mean_test_score", "std_test_score"]
-    #columns_csp=["param_CSP__log", "param_CSP__n_components", "param_CSP__norm_trace", "param_CSP__reg", "params", "mean_test_score", "std_test_score"]
     logging.basicConfig(filename="example.log", filemode="w", level=logging.DEBUG, format='%(asctime)s %(message)s')
-    
-    dataset = "/DATA4/"
+    # Suprimir la advertencia de convergencia    
+    dataset = "/DATAQ/"
     folder_input = "Epoch" + dataset
-    folder_output= "Output" + dataset
-    
-    #logging.info("LDA")
-    param_grid = {}
-    columns =[ "params", "mean_test_score", "std_test_score"]
-    ML = "CSP"
-    
+    folder_output= "Output6" + dataset
     run(folder_input, folder_output)
 
 if __name__ == "__main__":
